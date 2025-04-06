3e8f3b251b9933cf431343086ebe2c4c94fe8fb1
Sumeet Gaglani <sumeet942@gmail.com>
2025-04-06 19:13:19 -0400

    First commit

---
+++ dbt_postgres_local/models/example/my_first_dbt_model.sql
@@ -0,0 +1,17 @@
+{{ 
+    config(
+        materialized='table',
+        schema ='staging'
+    ) 
+}}
+
+with source_data as (
+
+    select 1 as id
+    union all
+    select 2 as id
+
+)
+
+select *
+from source_data
